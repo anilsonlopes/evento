fc6d8e3928fc183089c147768a873b1bdf3ada8f
Anilson Lopes <nissobmx@gmail.com>
2024-06-08 17:31:13 -0300

    ♻️ remove unused CSS classes from app and default components

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,4 +6,14 @@
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style lang="postcss">
+.form-label {
+  @apply block text-gray-600 mb-2;
+}
+.form-input-text {
+  @apply border rounded px-3 py-2 block outline-none w-full;
+}
+.form-input-text:focus {
+  @apply border-sky-600;
+}
+</style>
