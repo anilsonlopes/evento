bd43df28a3da90925d7c93cee17016f150f8e520
Anilson Lopes <nissobmx@gmail.com>
2024-06-09 00:39:46 -0300

    ✨ add dayjs-nuxt module for localized date handling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content"]
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content", "dayjs-nuxt"],
+
+  dayjs: {
+    locales: ["pt-br"],
+    defaultLocale: "pt-br",
+  },
+});
