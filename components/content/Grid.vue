25e31ceed19a3c8548e397730954abab13a5e14a
Anilson Lopes <nissobmx@gmail.com>
2024-06-08 21:59:29 -0300

    ✨ format and bind about content

---
+++ components/content/Grid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid lg:grid-cols-2 gap-10">
+    <div>
+      <ContentSlot :use="$slots.default" />
+    </div>
+    <div class="grid md:grid-cols-2 gap-10">
+      <ContentSlot :use="$slots.items" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ components/content/GridItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Icon :name="icon" size="3rem" />
+    <h3>
+      {{ title }}
+    </h3>
+    <p class="text-gray-500">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string
+  description: string
+  icon: string
+}>()
+</script>
+
+<style>
+
+</style>
