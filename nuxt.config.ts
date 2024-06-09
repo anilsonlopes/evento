c06026b79390f94a3a5bd902bcfb03393f324e1d
Anilson Lopes <nissobmx@gmail.com>
2024-06-09 03:25:49 -0300

    🚧 add supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content", "dayjs-nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxt/content",
+    "dayjs-nuxt",
+    "@nuxtjs/supabase"
+  ],
 
   dayjs: {
     locales: ["pt-br"],
     defaultLocale: "pt-br",
     plugins: ["localizedFormat"]
   },
+
+  supabase: {
+    key: process.env.SUPABASE_KEY,
+    url: process.env.SUPABASE_URL,
+    redirect: false,
+  }
 });
