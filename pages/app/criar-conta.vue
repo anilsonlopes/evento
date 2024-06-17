220f8213600237632d28577489479f6e47de61a7
Anilson Lopes <nissobmx@gmail.com>
2024-06-17 02:05:12 -0300

    🔥 remove form signup

--- pages/app/criar-conta.vue
+++ pages/app/criar-conta.vue
@@ -2,44 +2,26 @@
   <section>
     <div class="container mx-auto px-10 py-20 grid lg:grid-cols-4 gap-20">
       <div class="lg:col-span-2">
-        <h2 class="text-2xl lg:text-3xl font-bold">Crie sua conta</h2>
+        <h2 class="text-2xl lg:text-3xl font-bold">Acesse a plataforma</h2>
         <p class="mt-3 text-lg text-gray-800">
-          Crie sua conta e comece a usar o sistema agora mesmo. É rápido e
-          fácil.
+          Utilize seu provedor de preferencia para se autenticar aos eventos.
         </p>
       </div>
       <div class="lg:col-span-2">
         <Card>
           <CardHeader>
-            <div class="text-center text-sm">
-              Já possui uma conta?
-              <a href="#" class="underline"> Faça login </a>
-            </div>
+            <div class="text-center text-sm">Selecione seu provedor</div>
           </CardHeader>
-          <CardContent>
-            <div class="grid gap-4">
-              <div class="grid gap-2">
-                <Label for="name">Nome</Label>
-                <Input id="name" placeholder="Nome" required />
-              </div>
-              <div class="grid gap-2">
-                <Label for="email">E-mail</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div class="grid gap-2">
-                <Label for="password">Senha</Label>
-                <Input id="password" type="password" />
-              </div>
-              <Button type="submit" class="w-full"> Criar uma conta</Button>
-              <Button variant="outline" class="w-full">
-                Entrar com Zoho Auth
-              </Button>
-            </div>
+          <CardContent class="space-y-5">
+            <Button variant="outline" class="w-full">
+              Entrar com Zoho Auth
+            </Button>
+            <Button variant="outline" class="w-full">
+              Entrar com GitHub
+            </Button>
+            <Button variant="outline" class="w-full">
+              Entrar com conta Google
+            </Button>
           </CardContent>
         </Card>
       </div>
